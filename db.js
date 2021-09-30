d54d018e575f9d1f47ccae1170b0223ade57cdf4
Chris Blake <cblake2335@gmail.com>
2021-09-30 18:38:50 -0400

    Fixed db file

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, `${encodeURIComponent(process.env.DB_PASS)}`, {
-    host: process.env.DB_HOST,
+// const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, `${encodeURIComponent(process.env.DB_PASS)}`, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres'
+// })
+
+
+const sequelize = new Sequelize(process.env,DATABASE_URL, {
     dialect: 'postgres'
 })
+
+
+
+
 
 module.exports = {
     sequelize,
