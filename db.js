42f469108d4340a1a45be3a392ef097ef6902d55
Chris Blake <cblake2335@gmail.com>
2021-09-30 18:03:02 -0400

    Minor changes on db file

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USER, `${encodeURIComponent(process.env.DB_PASS)}`, {
     host: process.env.DB_HOST,
     dialect: 'postgres'
 })
