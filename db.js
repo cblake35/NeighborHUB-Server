b086ed0582cf1ed80648d3703f81f03780253d81
Chris Blake <cblake2335@gmail.com>
2021-09-30 18:59:34 -0400

    Fixed bug on db file

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const { Sequelize } = require('sequelize')
 // })
 
 
-const sequelize = new Sequelize(process.env,DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 })
 
